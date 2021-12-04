99806e5536a87a059b44da0561cbcb8c11321a32
Tom Kerswill <tom@cloud-bridge.co.uk>
2021-12-04 16:16:59 +0000

    New version

--- importToNewColumns.js
+++ importToNewColumns.js
@@ -14,6 +14,7 @@
 document.getFormNamed('Script Manager').runScriptNamed('Logger');
 
 function importToNewColumns(form, uri, indexKey) {
+    const indexKeyId = form.getFieldNamed(indexKey).getId();
 
     const jsonFile = uri;
     //    `	console.log("File: " + jsonFile);
